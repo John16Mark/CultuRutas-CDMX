147c3deac99d9adbf93d46797b351d3c9902dc58
dngxue <paolareyes802@gmail.com>
2025-06-08 22:34:20 -0600

    primeros cambios en home

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+const home = () => {
+
+  return (
+  <div className='hola'>
+    <p>hola a tdos</p>
+  </div>
+  );
+};
+
+export default home;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from '../pages/register/Register';
 import Login from '../pages/login/Login';
 import LugarDetalles from '../pages/lugarDetalles/LugarDetalles';
+import Home from '../pages/home/home';
 
 function AppRouter() {
   return(
@@ -10,8 +11,9 @@ function AppRouter() {
       <Routes>
         <Route path='/register' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
-        <Route path='/' element={<LugarDetalles/>} />
-          
+        <Route path='/lugar-detalles' element={<LugarDetalles/>} />
+        <Route path='/' element={<Home/>} />
+
       </Routes>
     </Router>
   )
